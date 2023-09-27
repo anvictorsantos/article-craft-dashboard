c10e57ff15bcd0b9de568b11c44a754985b08769
Antonio Pontes <anvictorsantos@gmail.com>
2023-09-27 20:33:26 -0300

    feat: local custom directive
    Adding autofocus custom directive.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,89 +1,29 @@
-<!-- Options API -->
-<!-- <script>
-export default {
-    data() {
-        return {
-            counter: 0
-        };
-    },
-    methods: {
-        increaseCounter() {
-            this.counter++;
-        },
-        decreaseCounter() {
-            this.counter--;
-        }
-    },
-    computed: {
-        myComputedProperty() {
-            return 'my result'
-        }
-    },
-    watch: {
-        count(newCount, oldCount) {
-            if (newCount == 20) alert ('count')
-        }
-    },
-    mounted() {
-        console.log('mounted')
-    },
-    unmounted() {
-        console.log('unmounted')
-    }
-};
-</script> -->
+<template>
+    <div class="home">
+        <h1 class="text-3xl font-bold underline">{{ counterData.title }}:</h1>
+        <div>
+            <button @click="decreaseCounter">-</button>
+            <span>{{ counterData.count }}</span>
+            <button @click="increaseCounter(2)">+</button>
+        </div>
 
-<!-- Compostion API (old pattern) -->
-<!-- <script>
-import { ref } from 'vue';
+        <p>This counter is {{ oddOrEven }}</p>
 
-export default {
-    setup() {
-        // property
-        const counter = ref(0);
-
-        // computed property
-        const increaseCounter = () => {
-            counter.value++;
-        };
-
-        const decreaseCounter = () => {
-            counter.value--;
-        };
-
-        // method property
-        return {
-            counter,
-            increaseCounter,
-            decreaseCounter
-        };
-    }
-};
-</script> -->
+        <div class="edit">
+            <h4>Edit counter title:</h4>
+            <input
+                v-model="counterData.title"
+                v-autofocus
+                type="text"
+            />
+        </div>
+    </div>
+</template>
 
 <script setup>
 // imports
 
-// import { ref } from 'vue';
-
-// // property
-// const counter = ref(0);
-// // two-way data biding
-// const counterTitle = ref('My Counter');
-
-import {
-    computed,
-    onActivated,
-    onBeforeMount,
-    onBeforeUnmount,
-    onBeforeUpdate,
-    onDeactivated,
-    onMounted,
-    onUnmounted,
-    onUpdated,
-    reactive,
-    watch
-} from 'vue';
+import { computed, onMounted, reactive, watch } from 'vue';
 
 // app title
 
@@ -124,56 +64,15 @@ const decreaseCounter = () => {
     counterData.count--;
 };
 
-// onBeforeMount(() => {
-//     console.log('onBeforeMount');
-// });
-
-// onBeforeUnmount(() => {
-//     console.log('onBeforeUnmount');
-// });
-
-// onUnmounted(() => {
-//     console.log('onUnmounted');
-// });
-
-// onActivated(() => {
-//     console.log('onActivated');
-// });
-
-// onDeactivated(() => {
-//     console.log('onDeactivated');
-// });
-
-// onBeforeUpdate(() => {
-//     console.log('onBeforeUpdate');
-// });
-
-// onUpdated(() => {
-//     console.log('onUpdated');
-// });
-
 onMounted(() => {
     console.log('Do stuff related to Counter');
 });
+
+// directives
+
+const vAutofocus = {
+    mounted: (el) => {
+        el.focus();
+    }
+};
 </script>
-
-<template>
-    <div class="home">
-        <h1 class="text-3xl font-bold underline">{{ counterData.title }}:</h1>
-        <div>
-            <button @click="decreaseCounter">-</button>
-            <span>{{ counterData.count }}</span>
-            <button @click="increaseCounter(2)">+</button>
-        </div>
-
-        <p>This counter is {{ oddOrEven }}</p>
-
-        <div class="edit">
-            <h4>Edit counter title:</h4>
-            <input
-                v-model="counterData.title"
-                type="text"
-            />
-        </div>
-    </div>
-</template>
