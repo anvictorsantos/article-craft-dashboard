3e507b26dc7155003b33bbe9d6952d5e606024dc
Antonio Pontes <anvictorsantos@gmail.com>
2023-09-30 09:00:31 -0300

    feat: router with composition api

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/posts',
+            name: 'posts',
+            component: () => import('../views/PostsView.vue')
+        },
+        {
+            path: '/postDetail/:id',
+            name: 'postDetail',
+            component: () => import('../views/PostDetailView.vue')
+        }
+    ]
+});
 
-export default router
+export default router;
---
+++ src/views/PostsView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="posts">
+        <h1>Posts</h1>
+
+        <ul>
+            <li>
+                <RouterLink to="/postDetail/id1">Post 1</RouterLink>
+                <RouterLink to="/postDetail/id2">Post 2</RouterLink>
+                <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+                <RouterLink to="/postDetail/id4">Post 4</RouterLink>
+            </li>
+        </ul>
+
+        <textarea v-autofocus></textarea>
+    </div>
+</template>
+
+<script setup>
+import { vAutofocus } from '../directives/vAutofocus';
+</script>
