af2b1465a98a6d6a41d33e83a27238015e56f36c
Antonio Pontes <anvictorsantos@gmail.com>
2023-10-02 19:12:25 -0300

    feat: post rendering with refs

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -3,11 +3,13 @@
         <h1>Posts</h1>
 
         <ul>
-            <li>
-                <RouterLink to="/postDetail/id1">Post 1</RouterLink>
-                <RouterLink to="/postDetail/id2">Post 2</RouterLink>
-                <RouterLink to="/postDetail/id3">Post 3</RouterLink>
-                <RouterLink to="/postDetail/id4">Post 4</RouterLink>
+            <li
+                v-for="post in posts"
+                :key="post.id"
+            >
+                <RouterLink :to="`/postDetail/${post.id}`">{{
+                    post.title
+                }}</RouterLink>
             </li>
         </ul>
 
@@ -16,5 +18,23 @@
 </template>
 
 <script setup>
+// imports
+import { ref } from 'vue';
 import { vAutofocus } from '../directives/vAutofocus';
+
+// mock posts
+const posts = ref([
+    {
+        id: 'id1',
+        title: 'Post 1'
+    },
+    {
+        id: 'id2',
+        title: 'Post 2'
+    },
+    {
+        id: 'id3',
+        title: 'Post 3'
+    }
+]);
 </script>
