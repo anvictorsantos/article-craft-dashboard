d951922a46e27a75b5e985ec1cb8c3e1c5746628
Antonio Pontes <anvictorsantos@gmail.com>
2023-10-05 19:13:44 -0300

    feat: slots

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>
+                <slot name="title" />
+            </h1>
+            <slot />
+            <button>Hide Modal</button>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+import { useSlots } from 'vue';
+
+const slots = useSlots();
+
+console.log(slots.title());
+</script>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,27 +2,22 @@
     <div class="modals">
         <h1>Modals</h1>
         <button @click="showModal = true">Show Modal</button>
-        <teleport to=".modals-container">
-            <div
-                v-if="showModal"
-                class="modal"
-            >
-                <h1>This is a modal</h1>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Debitis, accusamus alias quasi inventore voluptatem dolorem
-                    fugit cupiditate, molestiae ut earum reprehenderit a. Natus
-                    pariatur necessitatibus cumque iusto temporibus ullam ipsam.
-                </p>
-                <button @click="showModal = false">Hide Modal</button>
-            </div>
-        </teleport>
+        <ModalComponent v-if="showModal">
+            <template #title>This is a modal</template>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Debitis, accusamus alias quasi inventore voluptatem dolorem
+                fugit cupiditate, molestiae ut earum reprehenderit a. Natus
+                pariatur necessitatibus cumque iusto temporibus ullam ipsam.
+            </p>
+        </ModalComponent>
     </div>
 </template>
 
 <script setup>
 // imports
 import { ref } from 'vue';
+import ModalComponent from '@/components/ModalComponent.vue';
 
 // modals
 const showModal = ref(false);
